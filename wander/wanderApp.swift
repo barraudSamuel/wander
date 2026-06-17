94e991497f5004175bd5de31d7f6d365c34718b0
barraudSamuel <samuel.barraud1@gmail.com>
2026-06-17 15:15:04 +0700

    Initial Commit

---
+++ wander/wanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  wanderApp.swift
+//  wander
+//
+//  Created by Samuel Barraud on 17/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct wanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
